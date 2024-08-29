229f5096faed4fef963fe9bd0af3d5880ff0c08e
Augusto Gomes <119215064+GutoBsc@users.noreply.github.com>
2024-08-29 09:37:41 -0300

    Add files via upload

---
+++ aula21c.py
@@ -0,0 +1,11 @@
+def teste():
+    x = 8
+    print(f'Na função teste, n vale {n}.')
+    print(f'Na função teste, x vale {x}')
+
+
+# Programa teste
+n = 2
+print(f'Na função teste, n vale {n}.')
+teste()
+print(f'Na função teste, x vale {x}')
