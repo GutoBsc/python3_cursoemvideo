a5869abbab913cf52442a77a13a5c2482cf655ee
Augusto Gomes <119215064+GutoBsc@users.noreply.github.com>
2024-09-04 18:56:54 -0300

    Add files via upload

---
+++ aula22a.py
@@ -0,0 +1,5 @@
+import aula22a_uteis
+num = int(input('Digite um valor: '))
+print(f'O fatorial de {num} é {aula22a_uteis.fatorial(num)}')
+print(f'O dobro de {num} é {aula22a_uteis.dobro(num)}')
+print(f'O triplo de {num} é {aula22a_uteis.triplo(num)}')
